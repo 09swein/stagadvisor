e97e492481d04dbb18ee19bb82f40b987b1735d5
Steven Weinberg <stevenj.weinberg@gmail.com>
2013-11-10 14:42:15 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stagadvisor::Application.config.secret_key_base = 'a9a203aa94a6dd51d711725f0111dcdd839b0a1906511af85248ef40c2f41c764b9cd9fcf9da2658ce1e33b57846cfa74e4c15f554fb46c04937026bcc6bd6c6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Stagadvisor::Application.config.session_store :cookie_store, key: '_stagadvisor_session'
