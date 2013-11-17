f0edda84af34545232f8884addd16df2590b50c9
Steven Weinberg <stevenj.weinberg@gmail.com>
2013-11-17 12:33:49 -0500

    Reverse Comment Order via Place_Controller

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,5 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-	has_many :comments
+	has_many :comments, :order => "created_at DESC"
 end
