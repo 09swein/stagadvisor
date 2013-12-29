0bb6420d447eecaed650ba988bb4bb1196c8de73
Steven Weinberg <stevenj.weinberg@gmail.com>
2013-12-29 17:04:24 -0500

    Add Star Rating for all 4 categories and present in view

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,8 +1,19 @@
 module PlacesHelper
 
-	def show_stars(place)
-  	image_tag "5stars.png"
+	def show_attraction_stars(place)
+  	image_tag "#{place.attractionstars}stars.png"
 	end
 
+	def show_restaurant_stars(place)
+  	image_tag "#{place.restaurantstars}stars.png"
+	end
+
+	def show_hotel_stars(place)
+  	image_tag "#{place.hotelstars}stars.png"
+	end
+
+	def show_ladies_stars(place)
+  	image_tag "#{place.ladiesstars}stars.png"
+	end
 
 end
